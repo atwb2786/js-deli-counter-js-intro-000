a079525ee89584a19e65bad99627367a12b7c104
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 21:22:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
-  if (katzDeliLine.length === 0) {
+  if (line.length === 0) {
     return "The line is currently empty.";
   }
 
