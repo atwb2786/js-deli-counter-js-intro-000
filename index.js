c9c9e0a20eedf320045e68a169256faced519373
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 21:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(line) {
-  if (line.length === 0) {
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
 
   var customerNameAndNumber = [];
   for (var i = 0; i < katzDeliLine.length; i++) {
-    customerNameAndNumber.push(i + 1 + ". " + line[i]);
+    customerNameAndNumber.push(i + 1 + ". " + katzDeliLine[i]);
   }
   return `The line is currently: ${customerNameAndNumber.join(', ')}`;
 }
