1b7b77320e1c3ea79c651c24a2f61f59294b97bd
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-04 21:20:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(line) {
-  if (line.length === 0) {
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
 
@@ -23,5 +23,5 @@ function currentLine(line) {
   for (var i = 0; i < katzDeliLine.length; i++) {
     customerNameAndNumber.push(i + 1 + ". " + katzDeliLine[i]);
   }
-  return `The line is currently: ${customerNameAndNumber.join(", ")}`;
+  return `The line is currently: ${customerNameAndNumber.join(', ')}`;
 }
